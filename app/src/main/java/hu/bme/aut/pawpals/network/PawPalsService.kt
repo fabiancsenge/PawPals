acff346dcb2fe3ec06bac307e64a865653eee517
fabiancsenge <csengefabian@gmail.com>
2023-05-07 17:10:50 +0200

    Added service

--- app/src/main/java/hu/bme/aut/pawpals/network/PawPalsService.kt
+++ app/src/main/java/hu/bme/aut/pawpals/network/PawPalsService.kt
@@ -1,4 +1,50 @@
 package hu.bme.aut.pawpals.network
 
-class PawPalsService {
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.pawpals.network.api.PawPalsApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class PawPalsService {
+    companion object {
+        private const val IMG_URL = "https://dog.ceo/api"
+        private const val WALK_URL = "https://fakerapi.it/api/v1"
+
+        @Provides
+        @Singleton
+        fun provideMoshi(): Moshi = Moshi.Builder().build()
+
+        @Provides
+        @Singleton
+        fun provideOkHttpClient(): OkHttpClient {
+            return OkHttpClient.Builder().addNetworkInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            })
+                .build()
+        }
+
+        @Provides
+        @Singleton
+        fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(IMG_URL)
+                .client(okHttpClient)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+        }
+
+        @Provides
+        @Singleton
+        fun provideApi(retrofit: Retrofit): PawPalsApi = retrofit.create()
+    }
 }
